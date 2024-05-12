1894f607035ec5b728b9161869c7c0a9be475c4a
Philip Lee <philipleektx@gmail.com>
2024-05-12 22:24:30 +0800

    Incorporated schema reading from ini files

--- config/schema_registry.py
+++ config/schema_registry.py
@@ -10,25 +10,38 @@ It reads the configuration from the file and initializes the Schema Registry cli
 class KafkaSchemaRegistry:
 
     SCHEMA_CONFIG_API_PATH = "../resources/schema_config.ini"
-    SCHEMA_CONFIG_KEY = "SchemaRegistry"
-    SCHEMA_CONFIG_URL = "url"
+    SCHEMA_PATH = "../resources/schema.ini"
+    SCHEMA_HEADING = "Schema"
+    SCHEMA_KEY = "avro_schema_v1"
+    SCHEMA_CONFIG_HEADING = "SchemaRegistry"
+    SCHEMA_CONFIG_KEY = "url"
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initializes the KafkaSchemaRegistry object."""
         self._config = configparser.ConfigParser()
-        self._schema_registry = None
         self._schema_client = None
-        self._initialize_schema_registry()
-
-    def _initialize_schema_registry(self) -> None:
-        """Initializes the Schema Registry client."""
-        self._config.read(self.SCHEMA_CONFIG_API_PATH)
-        self._schema_registry = self._config.get(self.SCHEMA_CONFIG_KEY, self.SCHEMA_CONFIG_URL)
-        self._schema_client = SchemaRegistryClient({"url": self._schema_registry})
+        self._schema = ""
+        self._configure_schema_registry()
 
     @property
     def schema_client(self) -> SchemaRegistryClient:
         """Gets the Schema Registry client."""
         return self._schema_client
 
+    @property
+    def schema(self) -> str:
+        """Gets the Schema."""
+        return self._schema
+
+    @schema.setter
+    def schema(self, schema_path: str, schema_heading: str, schema_key: str) -> None:
+        """Sets the Schema from a specified file."""
+        self._config.read(schema_path)
+        self._schema = self._config.get(schema_heading, schema_key)
+
+    def _configure_schema_registry(self) -> None:
+        """Initializes the Schema Registry client."""
+        self._config.read(self.SCHEMA_CONFIG_API_PATH)
+        schema_registry = self._config.get(self.SCHEMA_CONFIG_HEADING, self.SCHEMA_CONFIG_KEY)
+        self._schema_client = SchemaRegistryClient({"url": schema_registry})
 
